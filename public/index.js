6bdcbb86047717f03501d993126528efd3ef0786
Clem <valles.clement@gmail.com>
2021-02-15 17:56:57 -0600

    index js service worker file

--- public/index.js
+++ public/index.js
@@ -1,4 +1,12 @@
-
+//directing to the service-worker file
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((reg) => {
+        console.log('Service worker registered.', reg);
+      });
+  });
+}
 
 let transactions = [];
 let myChart;
