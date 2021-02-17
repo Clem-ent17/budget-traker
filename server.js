2acb2277ea21acff07d55af59ae05e1d9f46cd01
Clem <valles.clement@gmail.com>
2021-02-16 18:13:10 -0600

    Trying to solve all my problems

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 //helps compress files to local
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
